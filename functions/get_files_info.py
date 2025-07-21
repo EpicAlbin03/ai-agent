42307d244e1c838793c0f7b1691599f9f0dbced4
EpicAlbin03qqqq <github.garter593@passmail.net>
2025-07-21 17:37:42 +0200

    get_file_content

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,27 +1,43 @@
 import os
+from config import MAX_CHARS
 
 def get_files_info(working_directory, directory="."):
-  dir_path = os.path.join(working_directory, directory)
-
-  if not os.path.isdir(dir_path):
-    return f'Error: "{directory}" is not a directory'
-
-  abs_path = os.path.abspath(dir_path)
   abs_working_path = os.path.abspath(working_directory)
+  target_path = os.path.abspath(os.path.join(working_directory, directory))
 
-  if not abs_path.startswith(abs_working_path):
+  if not os.path.isdir(target_path):
+    return f'Error: "{directory}" is not a directory'
+  if not target_path.startswith(abs_working_path):
     return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-  files_and_dirs = os.listdir(dir_path)
+  files_and_dirs = os.listdir(target_path)
 
   try:
     res = []
     for filename in files_and_dirs:
-      filepath = os.path.join(dir_path, filename)
+      filepath = os.path.join(target_path, filename)
       file_size = os.path.getsize(filepath)
       is_dir = os.path.isdir(filepath)
       res.append(f'- {filename}: file_size={file_size} bytes, is_dir={is_dir}')
-
     return '\n'.join(res)
   except Exception as e:
     return f'Error: {e}'
+
+def get_file_content(working_directory, file_path):
+  abs_working_path = os.path.abspath(working_directory)
+  target_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+  if not os.path.isfile(target_path):
+    return f'Error: File not found or is not a regular file: "{file_path}"'
+  if not target_path.startswith(abs_working_path):
+    return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+  try:
+      with open(target_path, "r") as f:
+        content = f.read(MAX_CHARS)
+        if len(content) >= MAX_CHARS:
+          return content + f'[...File "{file_path}" truncated at 10000 characters]'
+        return content
+  except Exception as e:
+    return f'Error: {e}'
+
--- tests.py
+++ tests.py
@@ -1,10 +1,11 @@
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_file_content
 
 def main():
-  print(get_files_info("calculator", "."), '\n')
-  print(get_files_info("calculator", "pkg"), '\n')
-  print(get_files_info("calculator", "/bin"), '\n')
-  print(get_files_info("calculator", "../"), '\n')
+  print(get_file_content("calculator", "lorem.txt"), '\n')
+  print(get_file_content("calculator", "main.py"), '\n')
+  print(get_file_content("calculator", "pkg/calculator.py"), '\n')
+  print(get_file_content("calculator", "/bin/cat"), '\n')
+  print(get_file_content("calculator", "pkg/does_not_exist.py"), '\n')
 
 if __name__ == "__main__":
     main()
