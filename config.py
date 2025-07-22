729def8bae3fe68b4636c0f64740c33aa95187df
EpicAlbin03qqqq <github.garter593@passmail.net>
2025-07-22 13:58:05 +0200

    ch4: agents

--- config.py
+++ config.py
@@ -1,2 +1,30 @@
 MAX_CHARS = 10000
 WORKING_DIR = "./calculator"
+MAX_ITERATIONS = 20
+
+SYSTEM_PROMPT = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+
+End all responses with a summary in an ordered list format.
+
+Example:
+'
+Final response:
+Okay, I have done this...
+
+1. **summary 1** abc...
+2. **summary 1** abc...
+3. **summary 1** abc...
+
+Summary of ordered list...
+'
+"""
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from google import genai
 from google.genai import types
 import sys
 from functions.schemas import available_functions, functions
-from config import WORKING_DIR
+from config import WORKING_DIR, MAX_ITERATIONS, SYSTEM_PROMPT
 
 
 def main():
@@ -23,43 +23,68 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-
     messages = [
         types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
-    system_prompt = """
-You are a helpful AI coding agent.
 
-When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+    call_model(client, messages, SYSTEM_PROMPT, prompt, verbose)
 
-- List files and directories
-- Read file contents
-- Execute Python files with optional arguments
-- Write or overwrite files
 
-All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-"""
-    res = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions], system_instruction=system_prompt
-        ),
-    )
-    prompt_tokens = res.usage_metadata.prompt_token_count
-    resp_tokens = res.usage_metadata.candidates_token_count
+def call_model(
+    client: genai.Client,
+    messages: list[types.Content],
+    system_prompt: str,
+    prompt: str,
+    verbose=False,
+):
+    try:
+        for i in range(MAX_ITERATIONS):
+            res = client.models.generate_content(
+                model="gemini-2.0-flash-001",
+                contents=messages,
+                config=types.GenerateContentConfig(
+                    tools=[available_functions], system_instruction=system_prompt
+                ),
+            )
 
-    if verbose:
-        print("User prompt:", prompt)
-        print("Prompt tokens:", prompt_tokens)
-        print("Response tokens:", resp_tokens, "\n")
-    for function_call_part in res.function_calls:
-        call_res = call_function(function_call_part, verbose)
-        resp = call_res.parts[0].function_response.response
-        if not resp:
-            raise Exception("Error: No response from function call")
-        else:
-            print(f"-> {resp}")
+            if not res.function_calls:
+                print(res.text)
+                break
+
+            prompt_tokens = res.usage_metadata.prompt_token_count
+            resp_tokens = res.usage_metadata.candidates_token_count
+            for candidate in res.candidates:
+                messages.append(candidate.content)
+
+            if verbose:
+                print("User prompt:", prompt)
+                print("Prompt tokens:", prompt_tokens)
+                print("Response tokens:", resp_tokens, "\n")
+
+            for function_call_part in res.function_calls:
+                call_res = call_function(function_call_part, verbose)
+                resp = call_res.parts[0].function_response.response
+
+                if not resp:
+                    raise Exception("Error: No response from function call")
+
+                messages.append(
+                    types.Content(
+                        role="tool",
+                        parts=[
+                            types.Part.from_function_response(
+                                name=call_res.parts[0].function_response.name,
+                                response=resp,
+                            )
+                        ],
+                    )
+                )
+
+        if i == MAX_ITERATIONS - 1:
+            print(f"Maximum iterations ({MAX_ITERATIONS}) reached.")
+            sys.exit(1)
+    except Exception as e:
+        raise Exception(f"Error: {e}")
 
 
 def call_function(function_call_part: types.FunctionCall, verbose=False):
